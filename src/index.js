6bd9f37ccc27568fa76a378685c56d1f2cd9745f
pulkitsujaan <pulkitsujaan2708@gmail.com>
2026-01-09 17:20:09 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const morgan = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const {rateLimit } = require('express-rate-limit')
+
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+})
+
+
+
+const app = express();
+
+const PORT = 3005;
+
+/** @type {import('http-proxy-middleware/dist/types').RequestHandler<express.Request, express.Response>} */
+const bookingProxy = createProxyMiddleware({
+  target: 'http://localhost:3002', // target host with the same base path
+  changeOrigin: true, // needed for virtual hosted sites
+});
+
+app.use(morgan('combined'));
+app.use(limiter);
+app.use('/bookingservice',async(req,res,next)=>{
+    console.log(req.headers['x-access-token']);
+    try {
+        const response = fetch('http://localhost:3001/api/v1/isauthenticated',{
+            headers:{
+                'x-access-token': req.headers['x-access-token']
+            }
+        })
+        console.log(response.data);
+        if(response.data.success){
+            next();
+        }
+        else{
+            return res.status(401).json({
+                message:'Unauthorised'
+            })
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message:"Unauthorised"
+        })
+    }
+})
+
+
+app.use('/bookingservice', bookingProxy);
+
+app.get('/home', (req,res) => {
+    return res.json({message: 'OK'});
+})
+
+app.listen(PORT,() => {
+    console.log(`Server started at port ${PORT}`);
+})
